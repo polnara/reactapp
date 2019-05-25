22c771aafb843ac0ac9d7826f61b9ad79ccfeec6
polnara <narsimcagips@gmail.com>
2019-05-25 21:53:54 +0530

    added api code

--- src/components/register.js
+++ src/components/register.js
@@ -1,22 +1,49 @@
 import React,{Component} from 'react';
 import '../App.css';
+import Axios from 'axios';
 
 export class Register extends React.Component {
+    constructor(){
+        super();
+        this.state = {'message':''};
+    }
+    handleRegister = event => {
+        console.log(this.state);
+
+        Axios.post('http://localhost:8020/productList/api/product',this.state)
+        .then(res => {
+            this.setState({'message':'Registerd successfully'})
+            console.log(res);
+        })
+        .catch(errmsg => {
+            console.log("Error message");
+        })
+
+    }
+    handleText = event =>{
+        this.setState({
+            [event.target.id]:event.target.value
+        })
+    }
     render(){
         return (
             <div>
-                <form id='form'>
-                    <input type="text" placeholder='Enter Name '/>
-                    <input type='text' placeholder = 'Enter Company'/>
-                    <select>
+                <div>
+                    {this.state.message}
+                </div>
+                <div className='form'>
+                    <input type="text" id='Name' placeholder='Enter Name ' onChange={this.handleText}/>
+                    <input type='text' id='Company' placeholder = 'Enter Company' onChange={this.handleText}/>
+                    <select id='Country' onChange={this.handleText}>
                         <option>India</option>
                         <option>USA</option>
                     </select>
-                    <input type='password' placeholder='Enter secreat key'/>
-                    <input type='number' placeholder='Enter warrenty period'/>
-                    <input type='radio'/>resell <input type='radio'/>Company
-                    <button>Register</button>
-                </form>
+                    <input type='password' id='Key' placeholder='Enter secreat key' onChange={this.handleText}/>
+                    <input type='number' id='warrentyPeriod' placeholder='Enter warrenty period' onChange={this.handleText}/>
+                    <input type='radio' name='type' id='type' value='resell' onChange={this.handleText}/>resell 
+                    <input type='radio' name='type' id='type' value='company' onChange={this.handleText}/>Company
+                    <button onClick={this.handleRegister}>Register</button>
+                </div>
             </div>
         )
     }
