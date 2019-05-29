0ef5c93034dd0c3f3265c82d9e36d6bf83321674
polnara <narsimcagips@gmail.com>
2019-05-29 06:42:45 +0530

    service logic for access data

--- src/components/product.js
+++ src/components/product.js
@@ -1,11 +1,32 @@
 import React,{Component} from 'react';
 import '../App.css';
+import Axios from 'axios';
 
 export class ProductComponent extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            responseData:[]
+        }
+    }
+    componentDidMount(){
+        Axios.get('http://localhost:8020/productList/api/product')
+        .then(res => {
+            console.log(res)
+            this.setState({responseData:res.data})
+        }).catch(err => {
+            console.log(err);
+        })
+
+        console.log("Component Did Mount called ")
+    }
     render(){
+        const { resData } = this.state;
+        console.log(resData)
         return (
             <div>
-                    It is Product Component
+                    this.state.responseData[0]
+                }
             </div>
         )
     }
